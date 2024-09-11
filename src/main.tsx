310cba070c41fc4d47e23256e8aa0f7f5a7f4a34
yp-23040708 <wu.dang@yeepay.com>
2024-09-11 16:07:44 +0800

    修改文件名称

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import {TonConnectUIProvider} from "@tonconnect/ui-react";
 
 createRoot(document.getElementById('root')!).render(
-    <TonConnectUIProvider manifestUrl="https://testnet-wallet.nal.network//tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://github.com/522923331/ton-wallet-react-demo/blob/master/manifest.json">
       <App />
     </TonConnectUIProvider>
 )
